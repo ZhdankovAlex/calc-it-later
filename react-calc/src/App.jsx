06d462ada4b9a93a62fa4642233d514d6312bff0
ZhdankovAlex <47736968+ZhdankovAlex@users.noreply.github.com>
2020-11-11 11:12:14 +0300

    all bugs was fixed

--- react-calc/src/App.jsx
+++ react-calc/src/App.jsx
@@ -10,7 +10,7 @@ class App extends Component {
     super(props);
 
     this.state = {
-      input: ''
+      input: ""
     };
   }
 
@@ -27,35 +27,33 @@ class App extends Component {
     return (
       <div className="app">
         <div className="calc-wrapper">
-          <Input input={this.state.input}></Input>
-
+          <Input input={this.state.input} />
           <div className="row">
-            <Button handleClick={this.addToInput}> 7 </Button>
-            <Button handleClick={this.addToInput}> 8 </Button>
-            <Button handleClick={this.addToInput}> 9 </Button>
-            <Button handleClick={this.addToInput}> * </Button>
+            <Button handleClick={this.addToInput}>7</Button>
+            <Button handleClick={this.addToInput}>8</Button>
+            <Button handleClick={this.addToInput}>9</Button>
+            <Button handleClick={this.addToInput}>*</Button>
           </div>
           <div className="row">
-            <Button handleClick={this.addToInput}> 4 </Button>
-            <Button handleClick={this.addToInput}> 5 </Button>
-            <Button handleClick={this.addToInput}> 6 </Button>
-            <Button handleClick={this.addToInput}> - </Button>
+            <Button handleClick={this.addToInput}>4</Button>
+            <Button handleClick={this.addToInput}>5</Button>
+            <Button handleClick={this.addToInput}>6</Button>
+            <Button handleClick={this.addToInput}>-</Button>
           </div>
           <div className="row">
-            <Button handleClick={this.addToInput}> 1 </Button>
-            <Button handleClick={this.addToInput}> 2 </Button>
-            <Button handleClick={this.addToInput}> 3 </Button>
-            <Button handleClick={this.addToInput}> + </Button>
+            <Button handleClick={this.addToInput}>1</Button>
+            <Button handleClick={this.addToInput}>2</Button>
+            <Button handleClick={this.addToInput}>3</Button>
+            <Button handleClick={this.addToInput}>+</Button>
           </div>
           <div className="row">
-            <Button handleClick={this.addToInput}> . </Button>
-            <Button handleClick={this.addToInput}> 0 </Button>
-            <Button handleClick={() => this.handleEqual()}> = </Button>
-            <Button handleClick={this.addToInput}> / </Button>
+            <Button handleClick={this.addToInput}>.</Button>
+            <Button handleClick={this.addToInput}>0</Button>
+            <Button handleClick={() => this.handleEqual()}>=</Button>
+            <Button handleClick={this.addToInput}>/</Button>
           </div>
           <div className="row">
-            <ClearButton handleClear={() =>
-              this.setState({input: ""})}>
+            <ClearButton handleClear={() => this.setState({ input: "" })}>
               Clear
             </ClearButton>
           </div>
--- react-calc/src/components/Button.jsx
+++ react-calc/src/components/Button.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import './Button.css';
 
+//is a number (not[not_is_a_number]) or . or =
+//doesn't work for . or =
+//и не понимаю, почему так [ TO DO ]
 const isOperator = val => {
-  //is a number (not[not_is_a_number]) or . or =
-  //doesn't work for . or =
-  //и не понимаю, почему так [ TO DO ]
   return !isNaN(val) || val === "." || val === "=";
-}
+};
 
 //вернём jsx
-export const Button = props =>
+export const Button = props => (
 //es-6 интерполяция строк ``
-  <div className={`button-wrapper ${
+  <div
+    className={`button-wrapper ${
       //текст на кнопке
       //if-else, но короче
       isOperator(props.children) ? null : "operator"
     }`}
-    onClick={() => props.handleClick(props.children)}>
+    onClick={() => props.handleClick(props.children)}
+  >
     {props.children}
-  </div>;
+  </div>
+);
