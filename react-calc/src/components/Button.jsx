b6d41bcc7b4979b1c1acf0edc7a215d85fdf82e9
ZhdankovAlex <47736968+ZhdankovAlex@users.noreply.github.com>
2020-11-11 12:27:38 +0300

    clear TO DO list

--- react-calc/src/components/Button.jsx
+++ react-calc/src/components/Button.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import './Button.css';
 
 //is a number (not[not_is_a_number]) or . or =
-//doesn't work for . or =
-//и не понимаю, почему так [ TO DO ]
 const isOperator = val => {
   return !isNaN(val) || val === "." || val === "=";
 };
